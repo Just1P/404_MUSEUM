cb07c2d19f816c50e408e6ffda8dacecaee75f8f
Just1P <justin.pitra@gmail.com>
2026-06-23 15:32:59 +0200

    ✨ feat: Add Other layout to Notfound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,10 @@
-import Layout404B from "../layouts/Layout404B";
+import Layout404A from '../layouts/Layout404A';
+import Layout404B from '../layouts/Layout404B';
+import Layout404C from '../layouts/Layout404C';
 
-// const layouts = [Layout404A, Layout404B, Layout404C];
-// // const Layout = layouts[Math.floor(Math.random() * layouts.length)];
+const layouts = [Layout404A, Layout404B, Layout404C];
+const Layout = layouts[Math.floor(Math.random() * layouts.length)];
 
 export default function NotFound() {
-  return <Layout404B />;
+  return <Layout />;
 }
